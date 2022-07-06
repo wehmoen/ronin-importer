374b3b5c665948945efd90136f21c6032a1476b5
Nico <mail@wehmoen.dev>
2022-07-07 00:44:25 +0200

    add command line options

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::fmt::Debug;
+use clap::Parser;
 use std::i64;
 use mongodb::{
     bson::doc,
@@ -9,6 +10,26 @@ use mongodb::sync::Collection;
 use serde::{Deserialize, Serialize};
 use web3::types::{BlockId, BlockNumber};
 
+/// Ronin blockchain importer for MongoDB
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    /// MongoDB Connection URL
+    #[clap(long, value_parser, default_value = "mongodb://127.0.0.1:27017")]
+    mongodb_uri: String,
+    /// MongoDB database
+    #[clap(long, value_parser, default_value = "roninrest")]
+    mongodb_name: String,
+    /// Web3 Websocket Host
+    #[clap(long, value_parser, default_value = "ws://localhost:8546")]
+    web3_hostname: String,
+    /// Start Block
+    #[clap(long, value_parser, default_value_t = 1)]
+    start_block: u32
+
+
+}
+
 #[derive(Debug, Serialize, Deserialize)]
 struct Transaction {
     sender: String,
@@ -18,11 +39,11 @@ struct Transaction {
 }
 
 #[tokio::main]
-async fn scan(col: Collection<Transaction>) -> web3::Result<()> {
-    let transport = web3::transports::WebSocket::new("ws://localhost:8546").await.unwrap();
+async fn scan(col: Collection<Transaction>, args: Args) -> web3::Result<()> {
+    let transport = web3::transports::WebSocket::new(&args.web3_hostname).await.unwrap();
     let web3 = web3::Web3::new(transport);
 
-    let mut block= web3::types::U64::from(1);
+    let mut block= web3::types::U64::from(args.start_block);
     let max_block = web3.eth().block_number().await.unwrap();
     loop {
         let block_data = web3.eth().block_with_txs(BlockId::Number(BlockNumber::from(block))).await.unwrap().unwrap();
@@ -55,12 +76,15 @@ async fn scan(col: Collection<Transaction>) -> web3::Result<()> {
 
 #[tokio::main]
 async fn main() -> Result<(), ()> {
-    let client = Client::with_uri_str("mongodb://127.0.0.1:27017").unwrap();
-    let database = client.database("roninrest");
+
+    let args: Args = Args::parse();
+
+    let client = Client::with_uri_str(&args.mongodb_uri).unwrap();
+    let database = client.database(&args.mongodb_name);
     let collection = database.collection::<Transaction>("transactions");
 
     tokio::task::spawn_blocking(|| {
-        scan(collection)
+        scan(collection, args)
     }).await.expect("Task panicked");
 
     Ok(())
