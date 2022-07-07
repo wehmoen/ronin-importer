790a4868fce7d1508a2894b703751663d679f819
Nico <mail@wehmoen.dev>
2022-07-07 10:42:33 +0200

    - add cli option to change mongo collection name - add fn to get latest block from db to resume importing - resume from latest db block if cli option --start-block is 0 - output result of scan() - print start and end block and wait 5 seconds before actually starting to sync

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,39 @@
+extern crate core;
+
 use std::fmt::Debug;
 use clap::Parser;
-use std::i64;
+use std::{i64, thread};
+use std::time::Duration;
 use mongodb::{
     bson::doc,
     bson::DateTime,
     sync::Collection,
     sync::Client,
+    options::FindOneOptions
 };
 use serde::{Deserialize, Serialize};
-use web3::types::{BlockId, BlockNumber, U64};
+use web3::types::{BlockId, BlockNumber};
 
 /// Ronin blockchain importer for MongoDB
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
 struct Args {
-    /// MongoDB Connection URL
+    /// MongoDB connection URL
     #[clap(long, value_parser, default_value = "mongodb://127.0.0.1:27017")]
     mongodb_uri: String,
-    /// MongoDB database
+    /// MongoDB database name
     #[clap(long, value_parser, default_value = "roninrest")]
     mongodb_name: String,
+    /// MongoDB collection name
+    #[clap(long, value_parser, default_value = "transactions")]
+    mongodb_collection: String,
     /// Web3 Websocket Host
     #[clap(long, value_parser, default_value = "ws://localhost:8546")]
     web3_hostname: String,
-    /// Start Block
+    /// Start Block - Set to 0 to resume from last block in database
     #[clap(long, value_parser, default_value_t = 1)]
     start_block: u32,
-    /// Endblock
+    /// End Block - If set to anything but 0 the import will stop at this block.
     #[clap(long, value_parser, default_value_t = 0)]
     end_block: u32
 }
@@ -39,17 +46,31 @@ struct Transaction {
     created_at: DateTime,
 }
 
+async fn get_db_head_block(col: &Collection<Transaction>) -> web3::types::U64 {
+    let options = FindOneOptions::builder().sort(doc! {"block": -1}).build();
+    let result: Transaction = col.find_one(None, options).unwrap().unwrap();
+    return web3::types::U64::from(result.block);
+}
+
 #[tokio::main]
 async fn scan(col: Collection<Transaction>, args: Args) -> web3::Result<()> {
     let transport = web3::transports::WebSocket::new(&args.web3_hostname).await.unwrap();
     let web3 = web3::Web3::new(transport);
 
-    let mut block= web3::types::U64::from(args.start_block);
-    let mut max_block = if args.end_block == 0 {
+    let mut block = if args.start_block == 0 {
+        get_db_head_block(&col).await + 1
+    } else {
+        web3::types::U64::from(args.start_block)
+    };
+    let max_block = if args.end_block == 0 {
         web3.eth().block_number().await.unwrap()
     } else {
         web3::types::U64::from(args.end_block)
     };
+
+    println!("Effective start: {}\tEffective end: {}", block, max_block);
+    println!("Continue Import in 5 seconds");
+    thread::sleep(Duration::from_secs(5));
     loop {
         let block_data = web3.eth().block_with_txs(BlockId::Number(BlockNumber::from(block))).await.unwrap().unwrap();
         let txs = block_data.transactions;
@@ -86,11 +107,18 @@ async fn main() -> Result<(), ()> {
 
     let client = Client::with_uri_str(&args.mongodb_uri).unwrap();
     let database = client.database(&args.mongodb_name);
-    let collection = database.collection::<Transaction>("transactions");
+    let collection = database.collection::<Transaction>(&args.mongodb_collection);
 
-    tokio::task::spawn_blocking(|| {
+    let scan_result = tokio::task::spawn_blocking(|| {
         scan(collection, args)
     }).await.expect("Task panicked");
+
+    let result = match scan_result {
+        Ok(_res) => std::string::String::from("Finished importing transactions!"),
+        Err(_error) => format!("{} {}",&"An error occured during the process of importing transactions!", _error)
+    };
+
+    println!("{}", result);
 
     Ok(())
 }
