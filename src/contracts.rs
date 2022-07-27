cbcdd97be75ab5104ce38cf34cf1f10646d1f7ff
Nico <mail@wehmoen.dev>
2022-07-27 17:24:33 +0200

    Add ERC20 and ERC721 Transfer indexer

---
+++ src/contracts.rs
@@ -0,0 +1,173 @@
+pub mod database {
+    use mongodb::bson::DateTime;
+    use crate::contracts::contracts::ContractType;
+    use serde::{Serialize, Deserialize};
+
+    #[derive(Serialize, Deserialize)]
+    pub struct Transfer {
+        pub from: String,
+        pub to: String,
+        pub token: String,
+        pub value_or_token_id: String,
+        pub created_at: DateTime,
+        pub block: u64,
+        pub transaction_id: String,
+        pub erc: ContractType
+    }
+
+    impl Transfer {
+        pub fn empty() -> Transfer {
+            Transfer {
+                from: "0x0000000000000000000000000000000000000000".to_string(),
+                to: "0x0000000000000000000000000000000000000000".to_string(),
+                token: "0x0000000000000000000000000000000000000000".to_string(),
+                value_or_token_id: "0".to_string(),
+                created_at: DateTime::from_millis(chrono::Utc::now().timestamp() * 1000),
+                block: 0u64,
+                transaction_id: "0x0000000000000000000000000000000000000000000000000000000000000000".to_string(),
+                erc: ContractType::Unknown
+            }
+        }
+    }
+}
+
+pub mod contracts {
+    use std::collections::HashMap;
+    use serde::{Serialize, Deserialize};
+
+    pub type ContractList = HashMap<&'static str, Contract>;
+
+    #[derive(Serialize, Deserialize)]
+    pub enum ContractType {
+        ERC20,
+        ERC721,
+        Unknown
+    }
+
+    #[derive(Serialize, Deserialize)]
+    pub struct Contract {
+        pub name: &'static str,
+        pub decimals: usize,
+        pub erc: ContractType,
+    }
+
+    pub fn default() -> ContractList {
+        let mut map: ContractList = HashMap::new();
+
+        map.insert("0xed4a9f48a62fb6fdcfb45bb00c9f61d1a436e58c", Contract {
+            name: "AXS",
+            decimals: 18,
+            erc: ContractType::ERC20,
+        });
+
+        map.insert("0xa8754b9fa15fc18bb59458815510e40a12cd2014", Contract {
+            name: "SLP",
+            decimals: 0,
+            erc: ContractType::ERC20,
+        });
+
+        map.insert("0x173a2d4fa585a63acd02c107d57f932be0a71bcc", Contract {
+            name: "AEC",
+            decimals: 0,
+            erc: ContractType::ERC20,
+        });
+
+        map.insert("0x0b7007c13325c48911f73a2dad5fa5dcbf808adc", Contract {
+            name: "USDC",
+            decimals: 18,
+            erc: ContractType::ERC20,
+        });
+
+        map.insert("0xe514d9deb7966c8be0ca922de8a064264ea6bcd4", Contract {
+            name: "WRON",
+            decimals: 18,
+            erc: ContractType::ERC20,
+        });
+
+        map.insert("0x32950db2a7164ae833121501c797d79e7b79d74c", Contract {
+            name: "AXIE",
+            decimals: 0,
+            erc: ContractType::ERC721,
+        });
+
+        map.insert("0x8c811e3c958e190f5ec15fb376533a3398620500", Contract {
+            name: "LAND",
+            decimals: 0,
+            erc: ContractType::ERC721,
+        });
+
+        map.insert("0xa96660f0e4a3e9bc7388925d245a6d4d79e21259", Contract {
+            name: "ITEM",
+            decimals: 0,
+            erc: ContractType::ERC721,
+        });
+
+        map
+    }
+}
+
+pub mod events {
+    use web3::ethabi::{Event, EventParam, ParamType};
+
+    // pub fn erc_20_burn() -> Event {
+    //     Event {
+    //         name: "Burn".to_string(),
+    //         inputs: vec![
+    //             EventParam {
+    //                 name: "_value".to_string(),
+    //                 kind: ParamType::Uint(256),
+    //                 indexed: true,
+    //             }
+    //         ],
+    //         anonymous: false,
+    //     }
+    // }
+
+    pub fn erc_20_transfer() -> Event {
+        Event {
+            name: "Transfer".to_string(),
+            inputs: vec![
+                EventParam {
+                    name: "_from".to_string(),
+                    kind: ParamType::Address,
+                    indexed: true,
+                },
+                EventParam {
+                    name: "_to".to_string(),
+                    kind: ParamType::Address,
+                    indexed: true,
+                },
+                EventParam {
+                    name: "_value".to_string(),
+                    kind: ParamType::Uint(256),
+                    indexed: false,
+                },
+            ],
+            anonymous: false,
+        }
+    }
+
+    pub fn erc_721_transfer() -> Event {
+        Event {
+            name: "Transfer".to_string(),
+            inputs: vec![
+                EventParam {
+                    name: "_from".to_string(),
+                    kind: ParamType::Address,
+                    indexed: true,
+                },
+                EventParam {
+                    name: "_to".to_string(),
+                    kind: ParamType::Address,
+                    indexed: true,
+                },
+                EventParam {
+                    name: "_tokenId".to_string(),
+                    kind: ParamType::Uint(256),
+                    indexed: true,
+                },
+            ],
+            anonymous: false,
+        }
+    }
+}
---
+++ src/erc-transfer.rs
@@ -0,0 +1,169 @@
+use clap::Parser;
+use hex_literal::hex;
+use mongodb::bson::{DateTime, doc};
+use mongodb::options::{FindOneOptions, InsertManyOptions};
+use mongodb::sync::{Client, Collection};
+use web3::ethabi::{Address, Event, RawLog};
+use web3::types::{BlockNumber, FilterBuilder};
+
+use crate::contracts::contracts::{Contract, ContractType};
+use crate::contracts::database::Transfer;
+
+mod contracts;
+
+/// Axie Infinity - Axie Transfer importer for MongoDB
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    /// MongoDB connection URL
+    #[clap(long, value_parser, default_value = "mongodb://127.0.0.1:27017")]
+    mongodb_uri: String,
+    /// MongoDB database name
+    #[clap(long, value_parser, default_value = "ronin")]
+    mongodb_name: String,
+    /// MongoDB collection name
+    #[clap(long, value_parser, default_value = "tokentransfers")]
+    mongodb_collection: String,
+    /// Web3 Websocket Host
+    #[clap(long, value_parser, default_value = "ws://localhost:8546")]
+    web3_hostname: String,
+    /// Web3 Provider Type
+    #[clap(long, value_parser, default_value = "ws")]
+    web3_provider_type: String,
+    /// Start Block - Set to 0 to resume from last block in database
+    #[clap(long, value_parser, default_value_t = 1)]
+    start_block: u32,
+    /// End Block - If set to anything but 0 the import will stop at this block.
+    #[clap(long, value_parser, default_value_t = 0)]
+    end_block: u32,
+}
+
+async fn get_db_head_block(col: &Collection<Transfer>) -> web3::types::U64 {
+    let options: FindOneOptions = FindOneOptions::builder().sort(doc! {"block": -1i64}).build();
+    let result = col.find_one(None, options).unwrap().unwrap_or(Transfer::empty());
+    return web3::types::U64::from(result.block);
+}
+
+
+#[tokio::main]
+async fn main() {
+    let args: Args = Args::parse();
+
+    let erc_20_transfer: Event = contracts::events::erc_20_transfer();
+    // let erc_20_burn: Event = contracts::events::erc_20_burn();
+    let erc_721_transfer: Event = contracts::events::erc_721_transfer();
+    let contracts: contracts::contracts::ContractList = contracts::contracts::default();
+
+
+    let transport = match args.web3_provider_type.as_str() {
+        "ws" => web3::transports::either::Either::Left(web3::transports::WebSocket::new(&args.web3_hostname).await.unwrap()),
+        "http" => web3::transports::either::Either::Right(web3::transports::Http::new(&args.web3_hostname).unwrap()),
+        _ => panic!("Invalid provider type")
+    };
+    let web3 = web3::Web3::new(transport);
+
+    let client = Client::with_uri_str(&args.mongodb_uri).unwrap();
+    let database = client.database(&args.mongodb_name);
+    let collection = database.collection::<Transfer>(&args.mongodb_collection);
+
+    let mut block = if args.start_block == 0 {
+        get_db_head_block(&collection).await + 1i32
+    } else {
+        web3::types::U64::from(args.start_block)
+    };
+
+    let max_block =
+        if args.end_block == 0 {
+            web3.eth().block_number().await.unwrap()
+        } else {
+            web3::types::U64::from(args.end_block)
+        };
+
+    loop {
+        let mut num_erc_20_transfers = 0;
+        let mut num_erc_721_transfers = 0;
+        let mut tx_pool: Vec<Transfer> = vec![];
+
+        for address in contracts.keys() {
+            let contract: &Contract = contracts.get(address).unwrap();
+
+            let address: Address = address.parse().unwrap();
+            let transfer_filter = FilterBuilder::default()
+                .from_block(BlockNumber::from(block))
+                .to_block(BlockNumber::from(block))
+                .address(vec![address])
+                .topics(
+                    Some(vec![hex!("ddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef").into()]),
+                    None,
+                    None,
+                    None,
+                ).build();
+
+            let filter = web3.eth_filter().create_logs_filter(transfer_filter).await.unwrap();
+            let result: Vec<web3::types::Log> = filter.logs().await.unwrap();
+
+            if result.len() > 0 {
+
+                for log in result {
+
+                    let raw_log = RawLog {
+                        topics: log.topics,
+                        data: log.data.0
+                    };
+
+                    let transfer: Transfer = match contract.erc {
+                        ContractType::ERC20 => {
+                            num_erc_20_transfers+=1;
+                            let data = erc_20_transfer.clone().parse_log(raw_log);
+                            let data = data.unwrap().params;
+
+                            Transfer {
+                                from: data[0].value.to_string(),
+                                to: data[1].value.to_string(),
+                                token: address.to_string(),
+                                value_or_token_id: data[2].value.to_string(),
+                                created_at: DateTime::from_millis(chrono::Utc::now().timestamp() * 1000),
+                                block: block.clone().as_u64(),
+                                transaction_id: web3::helpers::to_string(&log.transaction_hash.unwrap()),
+                                erc: ContractType::ERC20
+                            }
+                        }
+                        ContractType::ERC721 => {
+                            num_erc_721_transfers+=1;
+                            let data = erc_721_transfer.clone().parse_log(raw_log);
+                            let data = data.unwrap().params;
+
+                            Transfer {
+                                from: data[0].value.to_string(),
+                                to: data[1].value.to_string(),
+                                token: address.to_string(),
+                                value_or_token_id: data[2].value.to_string(),
+                                created_at: DateTime::from_millis(chrono::Utc::now().timestamp() * 1000),
+                                block: block.clone().as_u64(),
+                                transaction_id: web3::helpers::to_string(&log.transaction_hash.unwrap()),
+                                erc: ContractType::ERC721
+                            }
+                        }
+                        ContractType::Unknown => continue
+                    };
+
+                    tx_pool.push(transfer);
+                }
+            }
+        }
+
+        if tx_pool.len() > 0 {
+            let insert_options = InsertManyOptions::builder().ordered(false).build();
+            collection.insert_many(tx_pool, insert_options).ok();
+        }
+
+        println!("Block: {}\t\tERC20 Transfers: {}\tERC721 Transfers: {}", block, num_erc_20_transfers, num_erc_721_transfers);
+
+        block = block + 1i32;
+
+        if block > max_block {
+            println!("Breaking!");
+            break;
+        }
+    }
+}
